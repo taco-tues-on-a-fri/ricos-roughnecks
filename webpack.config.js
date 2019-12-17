0b7665dea7704f8c230f4a6c520426cb9adb258f
Taco <taco-tues@tuta.io>
2019-12-17 14:52:26 -0700

    fix typo in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader' ]}
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
