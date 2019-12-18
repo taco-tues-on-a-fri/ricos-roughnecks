8bd256c4bb7184c8f3e50eade1aa922fac088e5b
Taco <taco-tues@tuta.io>
2019-12-18 12:34:48 -0700

    begin building table component

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Container from '@material-ui/core/Container';
+import StickyHeadTable from './components/Table/Table'
 import './index.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Rico's Roughnecks!
-      </div>
+      <Container maxWidth="sm">
+        <StickyHeadTable />
+      </Container>
     )
   }
 }
