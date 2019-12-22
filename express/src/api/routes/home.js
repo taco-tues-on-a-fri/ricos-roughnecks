eda7f7dfea7b60a6eb294b13c60a206b1a1b8041
Taco <taco-tues@tuta.io>
2019-12-22 15:38:55 -0700

    testing a basic home end point

--- express/src/api/routes/home.js
+++ express/src/api/routes/home.js
@@ -5,7 +5,7 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
 // import { IUserInputDTO } from '../../interfaces/IUser';
-import middlewares from '../middlewares';
+// import middlewares from '../middlewares';
 // import { celebrate, Joi } from 'celebrate';
 
 const route = Router();
