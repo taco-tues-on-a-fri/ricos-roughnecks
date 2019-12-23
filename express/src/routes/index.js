4ccab5bcac0288418a408d8c31337469c29fa385
Taco <taco-tues@tuta.io>
2019-12-22 17:39:30 -0700

    test route

--- express/src/routes/index.js
+++ express/src/routes/index.js
@@ -1,8 +1,36 @@
-// routes/index.js
-import express from 'express';
-var router = express.Router();
+import { Router, Request, Response, NextFunction } from 'express';
+var router = Router();
+import { pool } from '../config/index'
+console.log(pool)
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  pool.query('SELECT * FROM person', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
 });
+
+
 export default router;
+
+
+// const addBook = (request, response) => {
+//   const { author, title } = request.body
+
+//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(201).json({ status: 'success', message: 'Book added.' })
+//   })
+// }
+
+// app
+//   .route('/books')
+//   // GET endpoint
+//   .get(getBooks)
+//   // POST endpoint
+//   .post(addBook)
