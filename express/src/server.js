269741345a0f603978fa1c438c3dcf496b1901a1
Taco <taco-tues@tuta.io>
2019-12-22 23:30:03 -0700

    src/server.js back to working state

--- express/src/server.js
+++ express/src/server.js
@@ -1,7 +1,15 @@
+//| Development notes
+//|------------------------------------------------------------------------
+//| 12-22-19: Reverting back to normal express structure
+//| 12-22-19: File is back to working state.
+
+
 import express from 'express';
 import path from 'path';
-import logger from 'morgan';
+import createError from 'http-errors'
+import morgan from 'morgan';
 import cors from 'cors';
+import Logger from './loaders/logger';
 // import cookieParser from 'cookie-parser';
 
 import { pool } from './config/index'
@@ -13,13 +21,56 @@ import router from './routes/index'
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/', router);
+
+app.get('/status', (req, res) => {
+  res.status(200).end()
+})
+app.head('/status', (req, res) => {
+  res.status(200).end()
+})
+
+  //| Catch 404 and forward to error handler
+//|------------------------------------------------------------------------
+
+//| Catch 404
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err['status'] = 404;
+  next(err);
+});
+
+//| Handle 401 thrown by express-jwt library
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    return res
+      .status(err.status)
+      .send({ message: err.message })
+      .end();
+  }
+  return next(err);
+});
+
+app.use((err, req, res, next) => {
+
+  //| include winston logging
+  Logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  Logger.error(`${err.stack === undefined ? '' : err.stack}`);
+
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+    },
+  });
+});
+
 
 
 
