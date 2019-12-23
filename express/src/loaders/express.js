a5ad13a7d41c0cfca71b61c85c07d1e5724bb0ba
Taco <taco-tues@tuta.io>
2019-12-22 19:38:35 -0700

    configured src/loaders/express.js

--- express/src/loaders/express.js
+++ express/src/loaders/express.js
@@ -1,50 +1,55 @@
-import express from 'express';
-import cors from 'cors';
-import routes from '../api';
-import config from '../config';
-export default ({ app }: { app: express.Application }) => {
-  /**
-   * Health Check endpoints
-   * @TODO Explain why they are here
-   */
+//|------------------------------------------------------------------------
+//| TODO: integrate morgan & winston
+
+import express from 'express'
+import cors from 'cors'
+import routes from '../api'
+import config from '../config'
+
+export default ({ app }) => {
+// export default ({ app }: { app: express.Application }) => {
+
+//| Health Check endpoints
+//|------------------------------------------------------------------------
   app.get('/status', (req, res) => {
-    res.status(200).end();
-  });
+    res.status(200).end()
+  })
   app.head('/status', (req, res) => {
-    res.status(200).end();
-  });
+    res.status(200).end()
+  })
 
-  // Useful if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-  // It shows the real origin IP in the heroku or Cloudwatch logs
-  app.enable('trust proxy');
 
-  // The magic package that prevents frontend developers going nuts
-  // Alternate description:
-  // Enable Cross Origin Resource Sharing to all origins by default
-  app.use(cors());
+//| Shows real origin IP in the heroku or Cloudwatch logs if behind a reverse proxy 
+//|------------------------------------------------------------------------
+  app.enable('trust proxy')
 
-  // Some sauce that always add since 2014
-  // "Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it."
-  // Maybe not needed anymore ?
-  app.use(require('method-override')());
 
-  // Middleware that transforms the raw string of req.body into json
-  app.use(bodyParser.json());
-  // Load API routes
+//| Enable Cross Origin Resource Sharing to all origins by default
+//|------------------------------------------------------------------------
+  app.use(cors())
+
+//| Transforms raw string of req.body into json
+  app.use(express.json())
+
+  app.use(express.urlencoded({ extended: true }))
+
+//| Load API routes
+//|------------------------------------------------------------------------
   app.use(config.api.prefix, routes());
 
-  /// catch 404 and forward to error handler
+
+  //| Catch 404 and forward to error handler
+//|------------------------------------------------------------------------
+
+//| Catch 404
   app.use((req, res, next) => {
     const err = new Error('Not Found');
     err['status'] = 404;
     next(err);
   });
 
-  /// error handlers
+//| Handle 401 thrown by express-jwt library
   app.use((err, req, res, next) => {
-    /**
-     * Handle 401 thrown by express-jwt library
-     */
     if (err.name === 'UnauthorizedError') {
       return res
         .status(err.status)
@@ -53,7 +58,11 @@ export default ({ app }: { app: express.Application }) => {
     }
     return next(err);
   });
+
   app.use((err, req, res, next) => {
+      //|  include winston logging
+      // winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+      // winston.error(`${err.stack === undefined ? '' : err.stack}`);
     res.status(err.status || 500);
     res.json({
       errors: {
