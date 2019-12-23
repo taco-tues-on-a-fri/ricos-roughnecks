db898c2416aaee9a05437751100a340875d09bf3
Taco <taco-tues@tuta.io>
2019-12-22 17:38:56 -0700

    integrate postgres into server.js

--- express/src/server.js
+++ express/src/server.js
@@ -1,15 +1,28 @@
-// app.js
 import express from 'express';
 import path from 'path';
-// import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-// import indexRouter from './routes/index';
+import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+
+import { pool } from './config/index'
+
+
 import router from './routes/index'
+// import indexRouter from './routes/index';
+
+
 const app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/', router);
+
+
+
 export default app;
+
+
