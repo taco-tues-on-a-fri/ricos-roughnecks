49db16f3dbb9500eb73f09de3bb801dd876aa380
Taco <taco-tues@tuta.io>
2019-12-22 19:36:48 -0700

    src/app.js successful connection

--- express/src/app.js
+++ express/src/app.js
@@ -8,21 +8,26 @@
 // import 'reflect-metadata'; // We need this in order to use @Decorators
 
 import config from './config';
-
+console.log('line11: src/app.js')
 import express from 'express';
+console.log('line13: src/app.js')
 
 import Logger from './loaders/logger';
+console.log('line16: src/app.js')
 
 async function startServer() {
+  console.log('line19: src/app.js')
   const app = express();
-
+  console.log('line21: src/app.js')
+  
   /**
    * A little hack here
    * Import/Export can only be used in 'top-level code'
    * Well, at least in node 10 without babel and at the time of writing
    * So we are using good old require.
    **/
-  await require('./loaders').default({ expressApp: app });
+  // await require('./loaders').default({ expressApp: app });
+  console.log('line: src/app.js')
 
   app.listen(config.port, err => {
     if (err) {
