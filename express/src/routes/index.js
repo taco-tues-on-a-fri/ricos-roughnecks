f8b6487e056fb3f55b5622b3652464d8b243eed6
Taco <taco-tues@tuta.io>
2019-12-23 20:49:33 -0700

    refactor

--- express/src/routes/index.js
+++ express/src/routes/index.js
@@ -1,43 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express';
 const router = Router();
 
-import { pool } from '../loaders/db'
-import { index } from '../controllers/index_controller'
+import { pool } from '../loaders/db';
+import { index } from '../controllers/index_controller';
 
-// GET | index
+//| GET | redirect root to /api/index
 //|------------------------------------------------------------------------
-// router.get('/home', index);
 router.get('/', function(req, res, next) {
   res.redirect('/api');
 });
 
-// router.get('/home', function(req, res, next) {
-//   pool.query('SELECT * FROM person', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     res.status(200).json(results.rows)
-//   })
-// });
-
-
 export default router;
-
-
-// const addBook = (request, response) => {
-//   const { author, title } = request.body
-
-//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(201).json({ status: 'success', message: 'Book added.' })
-//   })
-// }
-
-// app
-//   .route('/books')
-//   // GET endpoint
-//   .get(getBooks)
-//   // POST endpoint
-//   .post(addBook)
