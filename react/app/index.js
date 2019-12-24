29605b7a64879b84bda4bc7be06579357a115772
Taco <taco-tues@tuta.io>
2019-12-24 12:29:25 -0700

    remove ApiResponse from react/app/index.js

--- react/app/index.js
+++ react/app/index.js
@@ -4,30 +4,13 @@ require('dotenv').config()
 import Container from '@material-ui/core/Container';
 import StickyHeadTable from './components/Table/Table'
 import './index.css'
+import ApiResponse from './components/Api/ApiResponse'
 // import '../db/index.js'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/api/ticket")
-      .then(res => res.text())
-      // .then(res => res.JSON())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
   render() {
     return (
-      <Container>
-        <StickyHeadTable />
-      </Container>
+      <ApiResponse />
     )
   }
 }
