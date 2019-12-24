56a321dcd59d1229d651dc0e9119f19ca6f9e951
Taco <taco-tues@tuta.io>
2019-12-23 22:20:19 -0700

    add api test component

--- react/app/index.js
+++ react/app/index.js
@@ -4,14 +4,29 @@ require('dotenv').config()
 import Container from '@material-ui/core/Container';
 import StickyHeadTable from './components/Table/Table'
 import './index.css'
-import '../db/index.js'
+// import '../db/index.js'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/api/ticket")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
   render() {
     return (
-      <Container>
-        <StickyHeadTable />
-      </Container>
+      <div>
+        <p>{this.state.apiResponse}</p>
+      </div>
     )
   }
 }
@@ -20,3 +35,7 @@ ReactDom.render(
   <App />,
   document.getElementById('app')
 )
+
+// <Container>
+//         <StickyHeadTable />
+//       </Container>
