6de7f54fb521e8bbb650f2c0eb9909696ed4629f
Taco <taco-tues@tuta.io>
2019-12-23 20:46:52 -0700

    move code examples to api_controller

--- express/src/controllers/api_controller.js
+++ express/src/controllers/api_controller.js
@@ -21,3 +21,34 @@ export function index(req, res, next) {
 //       scrape_title: 'Create Scrape'
 //     });
 // };
+
+
+
+// const addBook = (request, response) => {
+//   const { author, title } = request.body
+
+//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(201).json({ status: 'success', message: 'Book added.' })
+//   })
+// }
+
+// router.get('/home', function(req, res, next) {
+//   pool.query('SELECT * FROM person', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     res.status(200).json(results.rows)
+//   })
+// });
+
+
+
+// app
+//   .route('/books')
+// GET endpoint
+//   .get(getBooks)
+// POST endpoint
+//   .post(addBook)
