a94747f1f0fd40c1d4047135c76101af2ce00239
Taco <taco-tues@tuta.io>
2019-12-27 17:11:03 -0700

    extract EditableCell to own Component

---
+++ react/app/components/Table/EditableCell.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// Create an editable cell renderer
+export default function EditableCell ({
+  cell: { value: initialValue },
+  row: { index },
+  column: { id },
+  updateMyData, // This is a custom function that we supplied to our table instance
+  editable,
+}) => {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = React.useState(initialValue)
+
+  const onChange = e => {
+    setValue(e.target.value)
+  }
+
+  // We'll only update the external data when the input is blurred
+  const onBlur = () => {
+    updateMyData(index, id, value)
+  }
+
+  // If the initialValue is changed externally, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  if (!editable) {
+    return `${initialValue}`
+  }
+
+  return <input value={value} onChange={onChange} onBlur={onBlur} />
+}
