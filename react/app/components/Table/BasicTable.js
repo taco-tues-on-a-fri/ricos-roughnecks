79f6c69b6bb14f47497659d5eb605eb36aeea1ac
Taco <taco-tues@tuta.io>
2019-12-29 20:44:44 -0700

    add basictable

---
+++ react/app/components/Table/BasicTable.js
@@ -0,0 +1,148 @@
+import React from 'react'
+import { EditableCell } from './EditableCell'
+import { IndeterminateCheckbox } from './IndeterminateCheckbox'
+import DefaultColumnFilter from './DefaultColumnFilter'
+import filterGreaterThan from './filterGreaterThan'
+import fuzzyTextFilterFn from './fuzzyTextFilterFn'
+import makeData from '../../../utils/makeData'
+import matchSorter from 'match-sorter'
+import NumberRangeColumnFilter from './NumberRangeColumnFilter'
+import roundedMedian from './roundedMedian'
+import SelectColumnFilter from './SelectColumnFilter'
+import SliderColumnFilter from './SliderColumnFilter'
+import styled from 'styled-components'
+import {
+  useTable,
+  usePagination,
+  useSortBy,
+  useFilters,
+  useGroupBy,
+  useExpanded,
+  useRowSelect,
+} from 'react-table'
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(
+          (row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )}
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+export default function BasicTable () {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'ticket_name',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(), [])
+  const madeData = makeData()
+  return (
+    // <Styles>
+    <React.Fragment>
+      {<div>
+        <pre>{JSON.stringify(madeData, null, 2)}</pre>
+      </div>}
+      {/* <Table columns={columns} data={data} /> */}
+    {/* </Styles> */}
+    </React.Fragment>
+  )
+}
