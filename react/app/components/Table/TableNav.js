38a63d8b170e0645a1b5e23991056447f4c13eae
Taco <taco-tues@tuta.io>
2019-12-31 09:37:25 -0700

    TableNav updated with hooks

--- react/app/components/Table/TableNav.js
+++ react/app/components/Table/TableNav.js
@@ -22,63 +22,117 @@ function QueryNav ({ selected, onUpdateQuery }) {
   )
 }
 
-export default class TableNav extends React.Component {
-  state = {
-    selectedQuery: 'Ticket',
-    query: {},
-    error: null
-  }
-  componentDidMount () {
-    this.updateQuery(this.state.selectedQuery)
-  }
-  updateQuery = (selectedQuery) => {
-    this.setState({
-      selectedQuery,
-      error: null
-    })
-    if (!this.state.query[selectedQuery]) {
-      fetchQuery(selectedQuery)
-        .then((data) => {
-          this.setState(({ query }) => ({
-            query: {
-              ...query, 
-              [selectedQuery]: data
-            }
-          }))
-        })  
-        .catch(() => {
-          console.warn('Error fetching query: ', error)
+// export class TableNavOriginal extends React.Component {
+//   state = {
+//     selectedQuery: 'Ticket',
+//     query: {},
+//     error: null
+//   }
+//   componentDidMount () {
+//     this.updateQuery(this.state.selectedQuery)
+//   }
+//   updateQuery = (selectedQuery) => {
+//     this.setState({
+//       selectedQuery,
+//       error: null
+//     })
+//     if (!this.state.query[selectedQuery]) {
+//       fetchQuery(selectedQuery)
+//         .then((data) => {
+//           this.setState(({ query }) => ({
+//             query: {
+//               ...query, 
+//               [selectedQuery]: data
+//             }
+//           }))
+//         })  
+//         .catch(() => {
+//           console.warn('Error fetching query: ', error)
 
-          this.setState({
-            error: `There was an error fetching the query.`
-          })
-        })
+//           this.setState({
+//             error: `There was an error fetching the query.`
+//           })
+//         })
+//     }
+//   }
+//   isLoading = () => {
+//     const { selectedQuery, query, error } = this.state
+
+//     return !query[selectedQuery] && error === null
+//   }
+//   render() {
+//     const { selectedQuery, query, error } = this.state
+
+//     return (
+//       <React.Fragment>
+//         <QueryNav
+//           selected={selectedQuery}
+//           onUpdateQuery={this.updateQuery}
+//         />
+//         {this.isLoading() && <p>LOADING</p>}
+
+//         {error && <p className='center-text error'>{error}</p>}
+
+//         {query[selectedQuery] && <GenerateTable 
+//           query={query[selectedQuery]}
+//           selectedQuery={selectedQuery}
+//           />
+//         }
+//       </React.Fragment>
+//     ) 
+//   }
+// }
+
+function tableNavReducer (state, action) {
+  if (action.type === 'success') {
+    return {
+      ...state,
+      [action.selectedQuery]: action.query,
+      error: null,
     }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      error: action.error.message
+    }
+  } else {
+    throw new Error(`That action type is not supported.`)
   }
-  isLoading = () => {
-    const { selectedQuery, query, error } = this.state
+}
 
-    return !query[selectedQuery] && error === null
-  }
-  render() {
-    const { selectedQuery, query, error } = this.state
+export default function TableNav () {
+  const [selectedQuery, setSelectedQuery] = React.useState('Ticket')
+  const [state, dispatch] = React.useReducer(
+    tableNavReducer,
+    { error: null }
+  )
 
-    return (
-      <React.Fragment>
-        <QueryNav
-          selected={selectedQuery}
-          onUpdateQuery={this.updateQuery}
-        />
-        {this.isLoading() && <p>LOADING</p>}
+  const fetchedQueries = React.useRef([])
 
-        {error && <p className='center-text error'>{error}</p>}
+  React.useEffect(() => {
+    if (fetchedQueries.current.includes(selectedQuery) === false) {
+      fetchedQueries.current.push(selectedQuery)
 
-        {query[selectedQuery] && <GenerateTable 
-          query={query[selectedQuery]}
-          selectedQuery={selectedQuery}
-          />
-        }
-      </React.Fragment>
-    ) 
-  }
+      fetchQuery(selectedQuery)
+        .then((query) => dispatch({ type: 'success', selectedQuery, query }))
+        .catch((error) => dispatch({ type: error, error }))
+    }
+  }, [fetchedQueries, selectedQuery])
+
+  const isLoading = () => !state[selectedQuery] && state.error === null
+
+  return (
+    <React.Fragment>
+      <QueryNav
+        selected={selectedQuery}
+        onUpdateQuery={setSelectedQuery}
+      />
+
+      {isLoading() && <p>LOADING</p>}
+
+      {state.error && <p className='center-text error'>{state.error}</p>}
+
+      {state[selectedQuery] && <GenerateTable query={state[selectedQuery]} />}
+    </React.Fragment>
+  )
 }
