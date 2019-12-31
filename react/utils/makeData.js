2a5d9ff41f279a0042ae2e59bc3d2c6c8e742771
Taco <taco-tues@tuta.io>
2019-12-31 09:09:44 -0700

    add makeData component

--- react/utils/makeData.js
+++ react/utils/makeData.js
@@ -1,58 +1,19 @@
-//| 12-29-19: Working Version.
+//| 12-29-19: 
 //|------------------------------------------------------------------------
-import namor from 'namor'
+import { fetchQuery } from './api'
+import util from 'util'
 
-const range = len => {
-  const arr = []
-  for (let i = 0; i < len; i++) {
-    arr.push(i)
-  }
-  return arr
-}
-
-const newPerson = () => {
-  const statusChance = Math.random()
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? 'relationship'
-        : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
-}
-
-export default function makeData(...lens) {
-  const makeDataLevel = (depth = 0) => {
-    const len = lens[depth]
-    return range(len).map(d => {
+export default function makeData() {
+  fetchQuery('ticket')
+  .then((query) => {
+    console.log('inside makeData: query')
+    console.log(util.inspect(query))
+    query.map((...column) => {
+      console.log('inside makeData: column')
+      console.log(util.inspect(column))
       return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+        ...column
       }
     })
-  }
-
-  return makeDataLevel()
-}
-
-const newRow = () => {
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? 'relationship'
-        : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
+  })
 }
