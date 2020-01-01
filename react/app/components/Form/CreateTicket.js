36b2beee1b04b0d6be886b4de9effa648c53a647
Taco <taco-tues@tuta.io>
2020-01-01 16:41:55 -0700

    form fields added

--- react/app/components/Form/CreateTicket.js
+++ react/app/components/Form/CreateTicket.js
@@ -21,10 +21,78 @@ function TicketForm({ onSubmit, label }) {
 
   return(
     <Form>
-      <Form.Group controlId="createTicket.ControlInput1">
-        <Form.Label>Ticket Name</Form.Label>
-        <Form.Control as="input" />
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridTicketName">
+          <Form.Label>Ticket Name</Form.Label>
+          <Form.Control as="input" placeholder="Enter Ticket Name"/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridType">
+          <Form.Label>Ticket Type</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>bug</option>
+            <option>improvement</option>
+            <option>new feature</option>
+            <option>task</option>
+            <option>custom issue</option>
+          </Form.Control>
+        </Form.Group>
+
+      </Form.Row>
+      
+      <Form.Group controlId="formGridDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" placeholder="Enter Ticket Description"/>
       </Form.Group>
+
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridProject">
+          <Form.Label>Project</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>Bug Tracker</option>
+            <option>Random Name Generator</option>
+            <option>Scraping Reddit</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridDeveloper">
+          <Form.Label>Assigned Developer</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>taco-tues-on-a-fri</option>
+            <option>willstall</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridPriority">
+          <Form.Label>Ticket Priority</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>blocker</option>
+            <option>critical</option>
+            <option>major</option>
+            <option>minor</option>
+            <option>trivial</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridStatus">
+          <Form.Label>Ticket Status</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>open</option>
+            <option>in progress</option>
+            <option>reopened</option>
+            <option>resolved</option>
+            <option>closed</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+
     </Form>
   )
 }
@@ -48,6 +116,18 @@ export default function CreateTicket() {
               /> 
             }
           </Col>
+
+          {ticket && (
+            <Link
+              className="btn dark-btn btn-space"
+              to={{
+                pathname: "/ticket/create",
+                search: `?ticket=${ticket}`
+              }}
+            >
+              Submit
+            </Link>
+          )}
         </Row>
       </Container>
     </React.Fragment>
