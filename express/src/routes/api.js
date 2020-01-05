8ceddf98f32e1aa7e3ddde7836bb335bc13305ff
Taco <taco-tues@tuta.io>
2020-01-05 09:55:57 -0700

    testing new endpoints

--- express/src/routes/api.js
+++ express/src/routes/api.js
@@ -1,9 +1,10 @@
-import { Router, Request, Response, NextFunction } from 'express';
-const router = Router();
-
-
-import { pool } from '../loaders/db';
-import { index, query_person, query_project, query_ticket } from '../controllers/api_controller';
+//| 01-04-20: Attempting to switch over to the loading configuration described in pg's documentation for express
+//|------------------------------------------------------------------------
+import { Request, Response, NextFunction } from 'express';
+import Router from 'express-promise-router'
+import db from '../loaders/db'
+import { index, query_person, query_project, query_ticket, create_ticket_get, create_ticket_post} from '../controllers/api_controller';
+const router = new Router()
 
 //| GET | index
 //|------------------------------------------------------------------------
@@ -13,4 +14,37 @@ router.get('/person', query_person);
 router.get('/project', query_project);
 router.get('/ticket', query_ticket);
 
-export default router;
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
+  res.send(rows[0])
+})
+
+export default router
+
+
+
+
+
+//| Original Setup
+//|------------------------------------------------------------------------
+// import { Request, Response, NextFunction } from 'express';
+
+// const router = Router();
+
+
+// import { pool } from '../loaders/db';
+// import { index, query_person, query_project, query_ticket, create_ticket_get, create_ticket_post} from '../controllers/api_controller';
+
+// //| GET | index
+// //|------------------------------------------------------------------------
+// router.get('/', index);
+
+// router.get('/person', query_person);
+// router.get('/project', query_project);
+// router.get('/ticket', query_ticket);
+
+// router.get('/ticket/create', create_ticket_form);
+// router.post('/ticket/create', create_ticket_post);
+
+// export default router;
