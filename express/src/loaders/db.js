86ec66c796ad308f7e48b84647386c4d8e3cd012
Taco <taco-tues@tuta.io>
2020-01-05 09:55:40 -0700

    change db connection method

--- express/src/loaders/db.js
+++ express/src/loaders/db.js
@@ -4,14 +4,12 @@
 //| 12-22-19: successfully created basic connection to db
 //| 12-22-19: TODO: Add db connection console.log
 //| 12-22-19: TODO: finish configuring and implement exports
-
-
-
+//|------------------------------------------------------------------------
+//| 01-04-20: Attempting to switch over to the loading configuration described in pg's documentation for express
 
 import { Pool } from 'pg'
 import dotenv from 'dotenv'
 import config from '../config'
-
 
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
@@ -21,6 +19,30 @@ const pool = new Pool({
   ssl: isProduction,
 })
 
+module.exports = {
+  pool,
+  query: (text, params) => pool.query(text, params),
+}
 
-module.exports = { pool }
+
+
+
+
+//| Original Setup
+//|------------------------------------------------------------------------
+// import { Pool } from 'pg'
+// import dotenv from 'dotenv'
+// import config from '../config'
+
+
+// const isProduction = process.env.NODE_ENV === 'production'
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//   ssl: isProduction,
+// })
+
+
+// module.exports = { pool }
 // export default pool 
