b4725d4bcf10107b4d9ddf62dc1e2af17a4dd054
Taco <taco-tues@tuta.io>
2020-01-05 13:22:11 -0700

    added TableNav route

--- react/app/index.js
+++ react/app/index.js
@@ -27,6 +27,7 @@ class App extends React.Component {
       <Router>
         <Switch>
           <Route exact path='/' component={FormikTicket} />
+          <Route path='/api/ticket/' component={TableNav} />
         </Switch>
       </Router>
       // <TableNav />
