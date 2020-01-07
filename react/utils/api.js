f31ff195ef898e354f5c666db1a144573d25db59
Taco <taco-tues@tuta.io>
2020-01-07 10:48:11 -0700

    add insertQuery function

--- react/utils/api.js
+++ react/utils/api.js
@@ -12,3 +12,61 @@ export function fetchQuery (query) {
       return data
     })
 }
+
+//| v.01 01-06-20
+//|------------------------------------------------------------------------
+// export function insertQuery (values) {
+//   // const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/${query}`)
+//   const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/?ticket_name=${values.ticket_name}&ticket_type=${values.ticket_type}&ticket_description=${values.ticket_description}&ticket_project=${values.ticket_project}&assigned_developer=${values.assigned_developer}&ticket_priority=${values.ticket_priority}&ticket_status=${values.ticket_status}`)
+//   console.log(endpoint)
+//   return fetch(endpoint)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (!data[0]) {
+//         throw new Error(data.message)
+//       }
+
+//       return data
+//     })
+// }
+//|------------------------------------------------------------------------
+
+
+//| v.02 01-06-20
+//|------------------------------------------------------------------------
+// export function insertQuery (values) {
+//   // const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/${query}`)
+//   const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/?ticket_name=${values.ticket_name}&ticket_type=${values.ticket_type}&ticket_description=${values.ticket_description}&ticket_project=${values.ticket_project}&assigned_developer=${values.assigned_developer}&ticket_priority=${values.ticket_priority}&ticket_status=${values.ticket_status}`)
+//   console.log(endpoint)
+//   return fetch(endpoint, {method: 'POST'})
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (!data[0]) {
+//         throw new Error(data.message)
+//       }
+
+//       return data
+//     })
+// }
+
+
+//| v.03 01-06-20
+//|------------------------------------------------------------------------
+export function insertQuery (values) {
+  // const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/${query}`)
+  const endpoint = window.encodeURI(`http://localhost:9000/api/ticket/`)
+  console.log(endpoint)
+  return fetch(endpoint, {
+    method: 'POST',
+    body: `?ticket_name=${values.ticket_name}&ticket_type=${values.ticket_type}&ticket_description=${values.ticket_description}&ticket_project=${values.ticket_project}&assigned_developer=${values.assigned_developer}&ticket_priority=${values.ticket_priority}&ticket_status=${values.ticket_status}`,
+    headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data[0]) {
+        throw new Error(data.message)
+      }
+
+      return data
+    })
+}
