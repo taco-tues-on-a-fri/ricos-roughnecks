9d1b416759f1e79929a7e16c4b836b864f34bb0b
Taco <taco-tues@tuta.io>
2020-01-07 10:46:31 -0700

    Add documentation, fixed column names

--- express/src/routes/api.js
+++ express/src/routes/api.js
@@ -1,15 +1,22 @@
 //| 01-04-20: Switched to the loading config described in node-postgres' express documentation
 //| 01-05-20: Dynamic general query of for the 3 main tables built
+//| 01-07-20: Saved build version 1.02 and reverting back to 1.01 after using
+//| 01-07-20:    {method: 'POST'} that introduced crashes
+//|------------------------------------------------------------------------
+//| 01-07-20: Post is now working, and all the table column names are correct
+//| 01-07-20: There is a problem with the construction of the insert query function
 //|------------------------------------------------------------------------
 
 import { Request, Response, NextFunction } from 'express';
 import Router from 'express-promise-router'
 import db from '../loaders/db'
+// import pool from '../loaders/db'
 const router = new Router()
 import queryString from 'query-string'
+import util from 'util'
 
 //| TODO: phase this controller out.
-import { index, query_person, query_project, query_ticket, create_ticket_get, create_ticket_post} from '../controllers/api_controller';
+import { index, query_person, query_ticket_project, query_ticket, create_ticket_get, create_ticket_post} from '../controllers/api_controller';
 
 
 //| GET | index
@@ -25,21 +32,24 @@ router.get('/:id', async (req, res) => {
   res.json(rows)
 });
 
-//| POST | post new ticket
+//| POST | v.01 post new ticket
 //|------------------------------------------------------------------------
-router.post('ticket/:id', async (req, res) => {
+router.post('/ticket', async (req, res) => {
+  console.log(util.inspect(queryString.parse(req.params.search)))
   const { 
-    ticketName,
-    ticketType,
-    ticketDescription,
-    project,
-    developer,
-    ticketPriority,
-    ticketStatus
+    ticket_name,
+    ticket_type,
+    ticket_description,
+    ticket_project,
+    assigned_developer,
+    ticket_priority,
+    ticket_status
   } = queryString.parse(req.params.search)
-  // const { rows } = await db.query(`INSERT INTO ticket(ticketName, ticketType, ticketDescription, project, developer, ticketPriority, ticketStatus) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`
-  const { rows } = await db.query(`INSERT INTO ticket(ticketName, ticketType, ticketDescription, project, developer, ticketPriority, ticketStatus) VALUES(${ticketName, ticketType, ticketDescription, project, developer, ticketPriority, ticketStatus}) RETURNING *`)
-  res.json(rows)
+  // const { rows } = await db.query(`INSERT INTO ticket(ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`
+  const { rows } = await db.query(`INSERT INTO ticket(ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status) VALUES(${ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status}) RETURNING *`)
+  .then(res => res.json(rows))
+  .catch(err => console.error('error executing query', err.stack))
+  // res.json(rows)
 });
 
 
@@ -48,57 +58,24 @@ export default router
 
 
 
-
-// router.get('/:id', async (req, res) => {
-//   const { id } = req.params
-//   const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-//   res.send(rows[0])
-// })
-
-
-
-
-
-
-
-
-//| Refactored after dynamic query function built, old code that was replaced below
+//| POST | v.02 post new ticket
 //|------------------------------------------------------------------------
-
-// router.get('/person', query_person);
-// router.get('/project', query_project);
-// router.get('/ticket', query_ticket);
-
-
-
-
-//| 
-//|------------------------------------------------------------------------
-//|------------------------------------------------------------------------
-
-
-
-
-
-//| Original Setup
-//|------------------------------------------------------------------------
-// import { Request, Response, NextFunction } from 'express';
-
-// const router = Router();
-
-
-// import { pool } from '../loaders/db';
-// import { index, query_person, query_project, query_ticket, create_ticket_get, create_ticket_post} from '../controllers/api_controller';
-
-// //| GET | index
-// //|------------------------------------------------------------------------
-// router.get('/', index);
-
-// router.get('/person', query_person);
-// router.get('/project', query_project);
-// router.get('/ticket', query_ticket);
-
-// router.get('/ticket/create', create_ticket_form);
-// router.post('/ticket/create', create_ticket_post);
-
-// export default router;
+// router.post('/ticket/:id', async (req, res) => {
+//   console.log(queryString.parse(req.params.search))
+  
+//   const { 
+//     ticket_name,
+//     ticket_type,
+//     ticket_description,
+//     ticket_project,
+//     assigned_developer,
+//     ticket_priority,
+//     ticket_status
+//   } = queryString.parse(req.params.search)
+//   const values = [`${ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status}`]
+//   const { rows } = await db.query(`INSERT INTO ticket(ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`)
+//   // const { rows } = await db.query(`INSERT INTO ticket(ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status) VALUES(${ticket_name, ticket_type, ticket_description, ticket_project, assigned_developer, ticket_priority, ticket_status}) RETURNING *`)
+//   .then(res => res.json(rows))
+//   .catch(err => console.error('error executing query', err.stack))
+//   // res.json(rows)
+// });
