d378b234701a4e7d86e71039c31665353839fe36
Taco <taco-tues@tuta.io>
2020-01-10 17:17:15 -0700

    complete refactor of app/index

--- react/app/index.js
+++ react/app/index.js
@@ -2,24 +2,11 @@ import React from 'react'
 import ReactDom from 'react-dom'
 require('dotenv').config() 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import TableNav from './components/Table/TableNav'
 import './index.css'
-import BugTable from './components/Table/BugTable'
-import BasicTable from './components/Table/BasicTable'
-import CreateTicket from './components/Form/CreateTicket'
-import BasicForm from './components/Form/BasicForm'
-import FormikBasic from './components/Form/FormikBasic'
-import FormikTicket from './components/Form/FormikTicket'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-// import Container from '@material-ui/core/Container';
-// import StickyHeadTable from './components/Table/Table'
-// import ApiResponse from './components/Api/ApiResponse'
-// import TestTable from './components/Table/TestTable'
-// import QueryTable from './components/Table/QueryTable'
-// import DynamicTable from './components/Table/DynamicTable'
-// import '../db/index.js'
-
+import TableNav from './components/Table/TableNav'
+import BugTable from './components/Table/BugTable'
+import FormikTicket from './components/Form/FormikTicket'
 
 class App extends React.Component {
   render() {
@@ -28,18 +15,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={TableNav} />
           <Route path='/create-ticket' component={FormikTicket} />
-          {/* <Route path='/tablenav' component={TableNav} /> */}
         </Switch>
       </Router>
-      // <TableNav />
-      // <FormikBasic />
-      // <BasicForm />
-      // <BasicTable />
-      // <CreateTicket />
-      // <TestTable />
-      // <QueryTable />
-      // <DynamicTable />
-      // <ApiResponse />
     )
   }
 }
@@ -48,81 +25,3 @@ ReactDom.render(
   <App />,
   document.getElementById('app')
 )
-
-
-//|------------------------------------------------------------------------
-// class App extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state={
-//       tableData:[
-//         {'fruit': 'Apple', 'cost': 100},
-//         {'fruit': 'Orange', 'cost': 50},
-//         {'fruit': 'Banana', 'cost': 35},
-//         {'fruit': 'Mango', 'cost': 70},
-//         {'fruit': 'Pineapple', 'cost': 45},
-//         {'fruit': 'Papaya', 'cost': 40},
-//         {'fruit': 'Watermelon', 'cost': 35}
-//       ],
-//       tableData2:[
-//         {'Name': 'Abc', 'Age': 15, 'Location': 'Bangalore'},
-//         {'Name': 'Def', 'Age': 43, 'Location': 'Mumbai'},
-//         {'Name': 'Uff', 'Age': 30, 'Location': 'Chennai'},
-//         {'Name': 'Ammse', 'Age': 87, 'Location': 'Delhi'},
-//         {'Name': 'Yysse', 'Age': 28, 'Location': 'Hyderabad'}
-//     ]
-//     }
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         Hello, React
-//         <br/> Table 1 data
-//         <QueryTable data={this.state.tableData}/>
-        
-//         <br/> Table 2 data
-//         <QueryTable data={this.state.tableData2}/>
-//       </div>
-//       // <ApiResponse />
-//       // <TestTable />
-//       // <TableJson data={this.state.tableData}/>
-//       // <QueryTable />
-//       // <DynamicTable />
-//     )
-//   }
-// }
-
-// ReactDom.render(
-//   <App />,
-//   document.getElementById('app')
-// )
-//|------------------------------------------------------------------------
-
-
-
-
-//| Original
-//|------------------------------------------------------------------------
-// class App extends React.Component {
-//   render() {
-//     return (
-//       // <ApiResponse />
-//       <TestTable />
-//       // <QueryTable />
-//       // <DynamicTable />
-//     )
-//   }
-// }
-
-// ReactDom.render(
-//   <App />,
-//   document.getElementById('app')
-// )
-//|------------------------------------------------------------------------
-// <Container>
-//         <StickyHeadTable />
-//       </Container>
-
-{/* <div>
-        <p>{this.state.apiResponse}</p>
-      </div> */}
